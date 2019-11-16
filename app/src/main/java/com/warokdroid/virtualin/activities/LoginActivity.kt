e6fbcd64b006e1e6a0b7378594feff18e2ef127e
Warok Droid <erikriosetiawan1997@gmail.com>
2019-11-17 05:48:38 +0700

    Add Firebase Authentication to Sign Up and Login Activity

--- app/src/main/java/com/warokdroid/virtualin/activities/LoginActivity.kt
+++ app/src/main/java/com/warokdroid/virtualin/activities/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.warokdroid.virtualin.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.warokdroid.virtualin.R
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
@@ -18,11 +25,14 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnLogin: Button
     private lateinit var tvSignUp: TextView
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         initViews()
+        initFirebase()
     }
 
     // Function to initialize the views
@@ -35,21 +45,46 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         tvSignUp = findViewById(R.id.tv_sign_up)
     }
 
+    private fun initFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
+    private fun login(email: String, password: String) {
+        if (TextUtils.isEmpty(email))
+            etEmail.error = "Email tidak boleh kosong"
+        else if (TextUtils.isEmpty(password))
+            etPassword.error = "Password tidak boleh kosong"
+        else {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this@LoginActivity, object : OnCompleteListener<AuthResult> {
+                    override fun onComplete(p0: Task<AuthResult>) {
+                        if (p0.isSuccessful) {
+                            val homeIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(homeIntent)
+                            finish()
+                            overridePendingTransition(
+                                R.transition.slide_in_right,
+                                R.transition.slide_out_left
+                            )
+                        } else
+                            showMessageBox("Login failed. Your username and password is not matched")
+
+                    }
+
+                })
+        }
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_login -> {
-                if (etEmail.text.toString().trim() == "warokdroid@gmail.com" && etPassword.text.toString().trim() == "WAROKDROID") {
-                    val homeIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(homeIntent)
-                    finish()
-                    overridePendingTransition(
-                        R.transition.slide_in_right,
-                        R.transition.slide_out_left
-                    )
-                } else {
-                    etEmail.error = "Email yang dimasukkan belum terdaftar"
-                    etPassword.error = "Password yang dimasukkan salah"
-                }
+                val email: String = etEmail.text.toString().trim()
+                val password: String = etPassword.text.toString().trim()
+                login(email, password)
+            }
+            else -> {
+                etEmail.error = "Email yang dimasukkan belum terdaftar"
+                etPassword.error = "Password yang dimasukkan salah"
             }
         }
     }
@@ -62,5 +97,19 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     fun onSignUp(view: View) {
         val registerIntent = Intent(this@LoginActivity, RegisterActivity::class.java)
         startActivity(registerIntent)
+    }
+
+    private fun showMessageBox(message: String) {
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Login")
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setCancelable(true)
+        alertDialogBuilder.setPositiveButton("OK", object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                dialog?.dismiss()
+            }
+
+        })
+        alertDialogBuilder.show()
     }
 }
--- app/src/main/java/com/warokdroid/virtualin/activities/RegisterActivity.kt
+++ app/src/main/java/com/warokdroid/virtualin/activities/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.warokdroid.virtualin.activities
 
-import android.content.Intent
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.warokdroid.virtualin.R
 
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,11 +25,14 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnCreateAccount: Button
     private lateinit var btnSignWithGoogle: Button
 
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         initViews()
+        initFirebase()
     }
 
     private fun initViews() {
@@ -38,18 +47,64 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         btnSignWithGoogle.setOnClickListener(this)
     }
 
+    private fun initFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_create_account -> {
-                val verificationIntent =
-                    Intent(this@RegisterActivity, VerificationActivity::class.java)
-                startActivity(verificationIntent)
+                val email: String = etEmail.text.toString().trim()
+                val password: String = etPassword.text.toString().trim()
+                signUp(email, password)
             }
 
             R.id.btn_sign_with_google -> {
                 Toast.makeText(this, "Button sign in with Google clicked!", Toast.LENGTH_SHORT)
                     .show()
             }
+        }
+    }
+
+    private fun signUp(email: String, password: String) {
+        if (TextUtils.isEmpty(email)) {
+            etEmail.error = "Email tidak boleh kosong"
+        } else if (TextUtils.isEmpty(password)) {
+            etPassword.error = "Password tidak boleh kosong"
+        } else {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                    override fun onComplete(p0: Task<AuthResult>) {
+                        if (p0.isSuccessful) {
+                            val alertDialogBuilder: AlertDialog.Builder =
+                                AlertDialog.Builder(this@RegisterActivity)
+                            alertDialogBuilder.setTitle("Sign Up")
+                            alertDialogBuilder.setMessage("Your account has been registered. Please sign in use your username and password.")
+                            alertDialogBuilder.setPositiveButton(
+                                "OK",
+                                object : DialogInterface.OnClickListener {
+                                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                                        this@RegisterActivity.finish()
+                                    }
+                                })
+                            alertDialogBuilder.setOnCancelListener(object :
+                                DialogInterface.OnCancelListener {
+                                override fun onCancel(dialog: DialogInterface?) {
+                                    this@RegisterActivity.finish()
+                                }
+                            })
+                            alertDialogBuilder.show()
+                        } else {
+                            p0.exception?.printStackTrace()
+                            Toast.makeText(
+                                this@RegisterActivity,
+                                "Registered has been failed! Please try again.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                })
         }
     }
 }
