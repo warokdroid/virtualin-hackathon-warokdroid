7e7b32be593bf988ffcfd547b665f1d8b8e5ff15
Warok Droid <erikriosetiawan1997@gmail.com>
2019-11-16 11:21:57 +0700

    Add SplashScreenActivity that showing a splash screen when the icon of app clicked

---
+++ app/src/main/java/com/warokdroid/virtualin/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.warokdroid.virtualin
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_TIME_OUT: Long = 2000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Using Handler to wait the next activity
+        Handler().postDelayed({
+
+            // Create intent object to move into LoginActivity
+            val loginIntent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(loginIntent)
+
+            // Call finish() function to destroy latest activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
